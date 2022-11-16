5cf8acd91234511149b3690241b81d61e22a7bcf
swpost12 <ftdslayer@gmail.com>
2022-11-16 12:47:59 -0600

    adding project backend frame work

---
+++ bookDraftingCenter/src/main/java/dev/post/bookDraftingCenter/BookDraftingCenterApplication.java
@@ -0,0 +1,13 @@
+package dev.post.bookDraftingCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDraftingCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDraftingCenterApplication.class, args);
+	}
+
+}
